166e6a0a4196f7a9b3721ad47f0181fd53393e5b
karim <karim@DESKTOP-KM53IJE>
2024-06-21 21:07:39 +0300

    front

--- Caffiee/wwwroot/js/site.js
+++ Caffiee/wwwroot/js/site.js
@@ -1,8 +1,11 @@
 ﻿$(document).ready(function () {
+    $('select').select2();
+
     $('.nav-link').click(function () {
         $('.nav-link').removeClass('active'); // Remove active class from all links
         $(this).addClass('active'); // Add active class to the clicked link
         $('.nav-link').css('border-color', 'transparent'); // Reset border color for all links
         $(this).css('border-color', 'green'); // Set border color to green for the clicked link
     });
+
 });
